7b39721acf5209ddfc5015b632080af73e6beba7
enzonjagi <amosndungo@gmail.com>
2021-06-06 14:40:45 +0300

    Python Basics[Lists]

---
+++ lists.py
@@ -0,0 +1,21 @@
+names = ["Njagi", "Ndungo", "Kalinawa"]
+print(names) 
+reverse = []
+
+#List methods
+"""
+numbers = [2,2,2,3,4,5,6]
+numbers.append(7)#add value to the end of a list
+numbers.insert(4, 4)#add value to a specific position, syntax list.insert(index, value)
+"""
+
+#reverse a list and print the revesed list
+#Solve this later
+reverse = []
+
+while names:
+    reverse.append(names[-1])
+"""names.remove(-1)
+    names[-1] = names[-2]
+"""    
+print (reverse)
