55bffb3f244ad29bae21363a24174c0bb670d58b
enzonjagi <amosndungo@gmail.com>
2021-06-06 08:39:14 +0300

    Python Basics[Strings]

---
+++ string.py
@@ -0,0 +1,8 @@
+course = "Python for beginners"
+print(course.upper()) #UPPER CASE 
+print(course.lower()) #lower case 
+print(course.find('n')) #returns index of the character n
+print(course.replace('for', '4'))#replace the string for with 4
+
+#check to see if the string Python exists in course, returns a boolean
+print('Python' in course) 
