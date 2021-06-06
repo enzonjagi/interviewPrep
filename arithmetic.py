50e40b7c88763a322e44c94705c1d0037313bf59
enzonjagi <amosndungo@gmail.com>
2021-06-06 08:47:01 +0300

    Python Basics[Arithmetic]

---
+++ arithmetic.py
@@ -0,0 +1,15 @@
+"""
+These operators include:
+    addition: +
+    subtraction: -
+    multiplication: *
+    division: 
+        / includes decimal points
+        // returns whole number only
+        % returns the remainder
+    augmented:
+        +=
+        -=
+        *=
+Please update if there's anything else missing
+"""
