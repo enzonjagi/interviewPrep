e225f518ec00d0adeac985e69ba66011dd86a220
enzonjagi <amosndungo@gmail.com>
2021-06-06 08:22:07 +0300

    Python Basics[2]

---
+++ type_conversion.py
@@ -0,0 +1,10 @@
+"""
+birth_year = input("When were you born mate: ")
+age = 2021 - int(birth_year)
+print("You are " + str(age) + " years old")
+"""
+#Assignment
+first = input("First: ")
+second = input("Second: ")
+sum = float(first) + float(second)
+print("Here's your sum mate: " + str(sum))
