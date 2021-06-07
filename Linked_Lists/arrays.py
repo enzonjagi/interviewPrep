fb520b2dae597a79e30551dd0841ad05a332c7be
enzonjagi <amosndungo@gmail.com>
2021-06-07 21:54:24 +0300

    Python Basics[Arrays Update]

--- Linked_Lists/arrays.py
+++ Linked_Lists/arrays.py
@@ -9,3 +9,18 @@ import array
 #variable = array.array(type_code, [array, items])
 a = array.array('i', [1,2,3,4,5,6])
 print(a)
+
+#Accessing Arrays
+#use index ordering to access items
+#for example
+print(a[-3]) #Should print the third-last item(4)
+
+#Array operations
+#length
+print(len(a))
+#adding: 
+# append(add 1 item), extend(add more than one), insert(add to specific position)
+a.append(7)
+a.insert(0, 0)
+a.extend([8,9,10])
+print(a)
