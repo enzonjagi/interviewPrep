d1f591b5bb33e5830f1c44e3dff62aa3a305719a
enzonjagi <amosndungo@gmail.com>
2021-06-07 21:43:05 +0300

    Python Basics[Arrays First]

---
+++ Linked_Lists/arrays.py
@@ -0,0 +1,11 @@
+#operations
+"""
+Importing
+    import array | import array as arr | from array import *
+"""
+import array
+
+#Creating array syntax
+#variable = array.array(type_code, [array, items])
+a = array.array('i', [1,2,3,4,5,6])
+print(a)
