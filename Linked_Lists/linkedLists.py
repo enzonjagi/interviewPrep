120e4c6a7b25d55e10c14e33f6bf247354112860
enzonjagi <amosndungo@gmail.com>
2021-06-07 21:08:56 +0300

    Python Basics[List Soln update]

--- Linked_Lists/linkedLists.py
+++ Linked_Lists/linkedLists.py
@@ -1,3 +1,4 @@
+#List Basics
 """
     Swap the first and last values of the list given,
     
@@ -14,3 +15,18 @@ last = l[-1]
 l[0] = last
 l[-1] = first
 print(l)
+
+"""
+Another Solution
+    Assign the first value to a variable
+    then refer the list position to the last value
+    then refer the last postion to the variable
+    See Below
+
+"""
+temp = l[0]
+l[0] = l[-1]
+l[-1] = temp
+print(l)
+
+#Linked List Practice
